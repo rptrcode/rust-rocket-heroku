06eafbabb7e4bffb45facc7c4767e89155e4f4fd
puttaraju <puttaraju.r@gmail.com>
2020-05-11 08:37:52 +0530

    rocket

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
-//! Example actix-web application.
-//!
-//! This code is adapted from the front page of the [Actix][] website.
-//!
-//! [actix]: https://actix.rs/docs/
+#![feature(proc_macro_hygiene, decl_macro)]
 
-use std::env;
+#[macro_use]
+extern crate rocket;
 
-use actix_web::{App, HttpRequest, Responder, server};
-
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
 fn main() {
-    // Get the port number to listen on.
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
-        .parse()
-        .expect("PORT must be a number");
-
-    // Start a server, configuring the resources to serve.
-    server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
-    })
-        .bind(("0.0.0.0", port))
-        .expect("Can not bind to port 8000")
-        .run();
+    rocket::ignite().mount("/", routes![index]).launch();
 }
